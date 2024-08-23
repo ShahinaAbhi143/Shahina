e1022315b3c4c418e278af99a64b99e8498e6ee3
Abhi Singh <156217394+ShahinaAbhi143@users.noreply.github.com>
2024-08-23 11:58:28 +0530

    Update purge.py

--- AnonXMusic/plugins/admins/purge.py
+++ AnonXMusic/plugins/admins/purge.py
@@ -3,7 +3,7 @@ from pyrogram import filters
 from pyrogram.enums import ChatType
 from pyrogram.errors import MessageDeleteForbidden, RPCError
 from pyrogram.types import Message
-from AnonXMusic.utils.daxx_ban import admin_filter
+from AnonXMusic.utils.Anony_ban import admin_filter
 from AnonXMusic import app
 
 
